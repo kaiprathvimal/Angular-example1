7fd2192d1fae71cf054533fa213b29c3ba28df9e
vimal kaiprath <vimalgithub@gmail.com>
2022-10-17 22:22:48 +0530

    css and component assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {ServerComponent} from "./Server/server.component";
 import {ServersComponent} from "./Servers/servers.component";
+import {WarningComponent} from "./Warning/warning.component";
+import {ErrorComponent} from "./Error/error.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
